7a7b5510f761f3f82ac7302adf15114ed12b0df9
christiangerbig <chr_gerbig@web.de>
2024-06-19 15:39:14 +0200

    update

--- sources/0_Wrapper.asm
+++ sources/0_Wrapper.asm
@@ -83,12 +83,12 @@ pt_v3.0b
   IFD pt_v3.0b
     INCLUDE "music-tracker/pt3-equals.i"
   ENDC
-pt_mute_enabled
 pt_ciatiming_enabled       EQU TRUE
 pt_finetune_enabled        EQU FALSE
   IFD pt_v3.0b
 pt_metronome_enabled       EQU FALSE
   ENDC
+pt_mute_enabled            EQU TRUE
 pt_track_volumes_enabled   EQU FALSE
 pt_track_periods_enabled   EQU FALSE
 pt_music_fader_enabled     EQU FALSE
